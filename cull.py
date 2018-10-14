02849446baec7c57085228c5e86af5fd5a21866f
jamesnicol <jamesnicol95@gmail.com>
2018-10-14 21:58:25 +1100

    added migrations file and updated cull file

--- cull.py
+++ cull.py
@@ -1,2 +1,7 @@
-from freshplaylist.views import cull_stale_tracks
-cull_stale_tracks()
+from freshplaylist.views import cull_stale_tracks, update_hitlists
+from freshplaylist.factory import create_app
+
+app = create_app()
+with app.app_context():
+    update_hitlists()
+    cull_stale_tracks()
